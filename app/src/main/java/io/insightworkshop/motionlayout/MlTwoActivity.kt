256953392de06f7c473b2524960bb8244c85542c
gprachan <70263211+gprachan@users.noreply.github.com>
2021-02-08 23:00:57 +0545

    [Add] trasition animation

---
+++ app/src/main/java/io/insightworkshop/motionlayout/MlTwoActivity.kt
@@ -0,0 +1,11 @@
+package io.insightworkshop.motionlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MlTwoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ml_two)
+    }
+}
